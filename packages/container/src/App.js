aa9717da26c8b5f208f9a0e00bf07e1d0a213b78
Nellickal Joy <arne@softwareag.com>
2021-03-25 15:41:34 +0530

    iniital commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarkettingApp from './components/markettingApp';
+
+
+export default ()=> {
+    return <div>
+        <h1> Hi There</h1>
+        <hr/>
+        <MarkettingApp/>
+    </div>
+};
