096474f5dbfa66e4ba1604644194cdd32c04f8bc
Nellickal Joy <arne@softwareag.com>
2021-04-14 13:27:23 +0530

    added dynamic classname

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
 import MarkettingApp from './components/markettingApp';
 import Header from './components/Header';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default ()=> {
     return(
+        
         <BrowserRouter>
-            <div>
-                <Header></Header>
-                <MarkettingApp/>
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header></Header>
+                    <MarkettingApp/>
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
