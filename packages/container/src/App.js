3a6154d1763bd5914ffb641037def46a0011e410
Nellickal Joy <arne@softwareag.com>
2021-04-26 20:00:24 +0530

    removed lazy for dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -7,9 +7,10 @@ import { createBrowserHistory } from 'history';
 import Progress from './components/progress';
 // import MarkettingApp from './components/markettingApp';
 // import AuthApp from './components/AuthApp';
+import DashboardApp from './components/DashboardApp';
 const MarketingLazy = lazy(() => import('./components/markettingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
-const DashboardLazy = lazy(()=> import('./components/DashboardApp'))
+// const DashboardLazy = lazy(()=> import('./components/DashboardApp'))
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
@@ -37,7 +38,7 @@ export default ()=> {
                             </Route>
                             <Route path='/dashboard'>
                                 {!isSignedIn && <Redirect to='/'/>}
-                                <DashboardLazy/>
+                                <DashboardApp/>
                             </Route>
                             <Route path='/'>
                                 <MarketingLazy/>
